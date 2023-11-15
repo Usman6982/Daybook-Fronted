4b890440ad38a5c8313bfc5c0fb83010a20fc017
Usman6982 <falakudaas4051@gmail.com>
2023-11-15 17:01:43 +0500

    Delete Note

--- src/Components/Noteitem.js
+++ src/Components/Noteitem.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import NoteContext from "../Context/notes/NoteContext"
+
 
 const Noteitem = (props) => {
+    const context = useContext(NoteContext); 
+    const { deleteNote } = context;
     const { note } = props;
     return (
         <div className="col-md-3">
@@ -8,7 +12,7 @@ const Noteitem = (props) => {
                     <div className="card-body">
                     <div className="d-flex align-items-centre">
                         <h5 className="card-title">{note.title}</h5>
-                        <i className="fa-regular fa-trash-can mx-2"></i>
+                        <i className="fa-regular fa-trash-can mx-2" onClick={()=>{deleteNote(note._id)}}></i>
                         <i className="far fa-edit mx-2"></i>
                         </div>
                         <p className="card-text">{note.description}</p>
